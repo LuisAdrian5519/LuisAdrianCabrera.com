6c378438a8cfa7c444095c509b47fc0e6cf1e756
LuisAdrian5519 <luisadrian.cabrera5519@gmail.com>
2025-11-06 01:08:54 -0800

    dot

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-  <footer className="bg-background border-t border-border py-12">
+  <footer className="bg-background border-t border-transparent py-6">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-6xl mx-auto">
-          <div className="mt-8 pt-8 border-t border-border text-center">
+          <div className="mt-4 border-t border-transparent text-center flex items-center justify-center py-4">
             <p className="text-muted-foreground text-sm text-center">
-              © {currentYear}. Luis Adrian Cabrera Muñoz
+              © {currentYear}. Luis Adrian Cabrera Muñoz.
             </p>
           </div>
         </div>
