0a8609ddba5a59c4b610359135f01381542e2c63
LuisAdrian5519 <luisadrian.cabrera5519@gmail.com>
2025-11-06 17:25:01 -0800

    Style

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import profMinato from "./assets/ProfMinato.jpeg";
+
+// Set favicon to ProfMinato image at runtime so the bundler provides the correct asset URL
+const setFavicon = (href: string) => {
+	let link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
+	if (!link) {
+		link = document.createElement("link");
+		link.rel = "icon";
+		document.head.appendChild(link);
+	}
+	link.href = href;
+};
+
+setFavicon(profMinato);
 
 createRoot(document.getElementById("root")!).render(<App />);
